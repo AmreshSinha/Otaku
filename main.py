7c1d2649e14337de8896905e79782b07aeb07dd6
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-07-21 16:45:19 +0530

    Init Commit

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ class Bot_Management(commands.Cog):
     async def donate(self, ctx):
         """Donation"""
 
-        embed=discord.Embed(title='Thanks for running this command!', color=0xc58c85)
+        embed=discord.Embed(title='Thanks for running this command!', description='.', color=0xc58c85)
         embed.add_field(name='Will add later! Till then Enjoy :)')
         embed.add_field(name='HF :)', inline=True)
         embed.set_thumbnail(url='https://media.giphy.com/media/S8qWh22ptNJNnEpBOf/giphy.gif')
